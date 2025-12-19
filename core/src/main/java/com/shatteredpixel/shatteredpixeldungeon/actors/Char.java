a209a19df08e4e014043b25b322647cb265a1256
Jun Lee <dan.junwoo.lee@gmail.com>
2025-12-19 15:08:13 -0500

    0.5.0-B5: hide damage number for Naginata

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/Char.java
@@ -440,7 +440,7 @@ public abstract class Char extends Actor {
 			enemy != Dungeon.hero && Dungeon.hero.belongings.weapon instanceof Naginata && this instanceof Hero &&
 				!enemy.properties().contains(Char.Property.BOSS) && !enemy.properties().contains(Char.Property.MINIBOSS)) {
 					sprite.showStatus(CharSprite.NEUTRAL, Messages.get(Naginata.class, "skill"));
-					enemy.damage(108108, this);
+					enemy.die(this);
 					SpellSprite.show(enemy, SpellSprite.FOOD);
 
 			}
