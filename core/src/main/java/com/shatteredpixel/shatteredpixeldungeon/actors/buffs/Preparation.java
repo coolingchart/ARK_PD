e03aa1816b8ba71eb0ff679f5249380a987c81c7
Jun Lee <dan.junwoo.lee@gmail.com>
2025-12-19 15:08:46 -0500

    0.5.0-B5: cannot select hero for preparation

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Preparation.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/Preparation.java
@@ -258,9 +258,9 @@ public class Preparation extends Buff implements ActionIndicator.Action {
 		public void onSelect(Integer cell) {
 			if (cell == null) return;
 			final Char enemy = Actor.findChar( cell );
-			if (enemy == null || Dungeon.hero.isCharmedBy(enemy) || enemy instanceof NPC || !Dungeon.level.heroFOV[cell]){
-				GLog.w(Messages.get(Preparation.class, "no_target"));
-			} else {
+            if (enemy == null || Dungeon.hero.isCharmedBy(enemy) || enemy instanceof NPC || !Dungeon.level.heroFOV[cell] || enemy == Dungeon.hero){
+                GLog.w(Messages.get(Preparation.class, "no_target"));
+            } else {
 
 				//just attack them then!
 				if (Dungeon.hero.canAttack(enemy)){
