2266dee55374e9e5c8069972b7875a2dd28851be
Jun Lee <dan.junwoo.lee@gmail.com>
2025-12-20 23:25:03 -0500

    0.5.0-B5-hotfix: version update

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/WelcomeScene.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/scenes/WelcomeScene.java
@@ -48,7 +48,7 @@ import java.util.Locale;
 
 public class WelcomeScene extends PixelScene {
 
-	private static final int LATEST_UPDATE = TomorrowRogueNight.v0_5_0_b5;
+	private static final int LATEST_UPDATE = TomorrowRogueNight.v0_5_0_b5_hotfix;
 
     //used so that the game does not keep showing the window forever if cleaning fails
     private static boolean triedCleaningTemp = false;
