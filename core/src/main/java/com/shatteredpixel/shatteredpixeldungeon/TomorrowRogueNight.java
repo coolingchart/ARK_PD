39de81ceea2e41e390075345c4d1faa8d9b911de
Jun Lee <dan.junwoo.lee@gmail.com>
2025-12-20 23:36:17 -0500

    0.5.0: version set up

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/TomorrowRogueNight.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/TomorrowRogueNight.java
@@ -36,6 +36,7 @@ public class TomorrowRogueNight extends Game {
 	public static final int v0_5_0_b4 = 583;
     public static final int v0_5_0_b5 = 600;
     public static final int v0_5_0_b5_hotfix = 602;
+    public static final int v0_5_0 = 603;
 	
 	public TomorrowRogueNight(PlatformSupport platform ) {
 		super( sceneClass == null ? WelcomeScene.class : sceneClass, platform );
