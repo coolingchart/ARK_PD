ba64981ba242d90d7bdde49d6a6f4225d186b481
Jun Lee <dan.junwoo.lee@gmail.com>
2026-03-19 08:45:19 -0400

    0.5.1: detach nervous impairment when 0

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/NervousImpairment.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/actors/buffs/NervousImpairment.java
@@ -19,6 +19,7 @@ public class NervousImpairment extends Buff {
 
     public void sum(float nervousDamage) {
         currentDamage = Math.min(100, currentDamage + nervousDamage);
+        if (currentDamage <= 0) detach();
         if (currentDamage >= limit) burst();
     }
 
