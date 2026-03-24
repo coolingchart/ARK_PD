5b447f4b749c4006dcd60d8c1bf68c971900d0a6
Jun Lee <dan.junwoo.lee@gmail.com>
2026-03-24 01:47:03 -0400

    0.5.1: rewardBox nullPointerException fix (#29)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/RandomBox.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/RandomBox.java
@@ -1,11 +1,7 @@
 package com.shatteredpixel.shatteredpixeldungeon.items;
 
-import com.shatteredpixel.shatteredpixeldungeon.Challenges;
 import com.shatteredpixel.shatteredpixeldungeon.Dungeon;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-import com.shatteredpixel.shatteredpixeldungeon.items.Skill.SkillBook;
-import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.Artifact;
-import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
 import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MeleeWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
@@ -16,6 +12,7 @@ import java.util.ArrayList;
 
 public class RandomBox extends Item {
     private static final String AC_ADD = "ADD";
+
     {
         image = ItemSpriteSheet.CHEST;
         stackable = true;
@@ -25,91 +22,99 @@ public class RandomBox extends Item {
     }
 
     @Override
-    public ArrayList<String> actions(Hero hero ) {
-        ArrayList<String> actions = super.actions( hero );
-        actions.add( AC_ADD );
+    public ArrayList<String> actions(Hero hero) {
+        ArrayList<String> actions = super.actions(hero);
+        actions.add(AC_ADD);
         return actions;
     }
 
     @Override
-    public void execute( Hero hero, String action ) {
+    public void execute(Hero hero, String action) {
 
-        super.execute( hero, action );
+        super.execute(hero, action);
 
-        if (action.equals( AC_ADD )) {
+        if (action.equals(AC_ADD)) {
 
-            hero.spend( 1 );
+            hero.spend(1);
             hero.busy();
 
-            hero.sprite.operate( hero.pos );
+            hero.sprite.operate(hero.pos);
 
-            detach( hero.belongings.backpack );
-            Randomreward(Random.IntRange(0,9)); }
+            detach(hero.belongings.backpack);
+            randomReward(Random.IntRange(0, 9));
         }
-
-
-    public void Randomreward(int i) {
-        if (i < 6) {
-            if (Random.Int(5) < 3) { GetGold_low();
-            } else GetGold_high(); }
-        else if (i < 8) { GetSkill(); }
-        else GetWeapon();
     }
 
-    public void GetGold_low()
-    { new Gold(Random.IntRange(150,650)).doPickUp(Dungeon.hero); }
 
-    public void GetGold_high()
-    { new Gold(Random.IntRange(300,1000)).doPickUp(Dungeon.hero); }
+    public void randomReward(int i) {
+        if (i < 6) {
+            if (Random.Int(5) < 3) {
+                getGoldLow();
+            } else getGoldHigh();
+        } else if (i < 8) {
+            getSkill();
+        } else getWeapon();
+    }
 
-    public void GetSkill(){
+    public void getGoldLow() {
+        new Gold(Random.IntRange(150, 650)).doPickUp(Dungeon.hero);
+    }
+
+    public void getGoldHigh() {
+        new Gold(Random.IntRange(300, 1000)).doPickUp(Dungeon.hero);
+    }
+
+    public void getSkill() {
         int chance = Random.IntRange(0, 50);
 
-        chance += Random.IntRange(-10,Dungeon.hero.lvl);
-        chance += Random.IntRange(0,Dungeon.hero.STR);
-        chance += Random.IntRange(0,Dungeon.depth);
+        chance += Random.IntRange(-10, Dungeon.hero.lvl);
+        chance += Random.IntRange(0, Dungeon.hero.STR);
+        chance += Random.IntRange(0, Dungeon.depth);
 
-        if (chance > 60)
-        {
-            Item n = Generator.random( Generator.Category.SKL_T3 );
+        if (chance > 60) {
+            Item n = Generator.random(Generator.Category.SKL_T3);
             Dungeon.level.drop(n, Dungeon.hero.pos).sprite.drop(Dungeon.hero.pos);
-        }
-        else if (chance > Random.IntRange(20,60)){
-            Item n = Generator.random( Generator.Category.SKL_T2 );
+        } else if (chance > Random.IntRange(20, 60)) {
+            Item n = Generator.random(Generator.Category.SKL_T2);
             Dungeon.level.drop(n, Dungeon.hero.pos).sprite.drop(Dungeon.hero.pos);
-        }
-        else { Item n = Generator.random( Generator.Category.SKL_T1 );
+        } else {
+            Item n = Generator.random(Generator.Category.SKL_T1);
             Dungeon.level.drop(n, Dungeon.hero.pos).sprite.drop(Dungeon.hero.pos);
         }
     }
 
-    public void GetWeapon() {
+    public void getWeapon() {
         int chance = Random.IntRange(0, 50);
 
         chance += Random.IntRange(-5, Dungeon.hero.STR);
-        chance += Random.IntRange(0, Dungeon.hero.belongings.weapon.buffedLvl() * 8);
+        chance += Random.IntRange(0, Dungeon.hero.belongings.weapon != null ? Dungeon.hero.belongings.weapon.buffedLvl() * 8 : 0);
 
         Weapon n;
 
-        if (chance > 65) { Generator.Category c = Generator.wepTiers[4];
-        n = (MeleeWeapon)Reflection.newInstance(c.classes[Random.chances(c.probs)]);}
-        else if (chance > 50) { Generator.Category c = Generator.wepTiers[3];
-            n = (MeleeWeapon)Reflection.newInstance(c.classes[Random.chances(c.probs)]);}
-        else if (chance > 35) { Generator.Category c = Generator.wepTiers[2];
-            n = (MeleeWeapon)Reflection.newInstance(c.classes[Random.chances(c.probs)]);}
-        else { Generator.Category c = Generator.wepTiers[1];
-            n = (MeleeWeapon)Reflection.newInstance(c.classes[Random.chances(c.probs)]);}
+        if (chance > 65) {
+            Generator.Category c = Generator.wepTiers[4];
+            n = (MeleeWeapon) Reflection.newInstance(c.classes[Random.chances(c.probs)]);
+        } else if (chance > 50) {
+            Generator.Category c = Generator.wepTiers[3];
+            n = (MeleeWeapon) Reflection.newInstance(c.classes[Random.chances(c.probs)]);
+        } else if (chance > 35) {
+            Generator.Category c = Generator.wepTiers[2];
+            n = (MeleeWeapon) Reflection.newInstance(c.classes[Random.chances(c.probs)]);
+        } else {
+            Generator.Category c = Generator.wepTiers[1];
+            n = (MeleeWeapon) Reflection.newInstance(c.classes[Random.chances(c.probs)]);
+        }
 
-        int upchacne = Random.IntRange(0,25);
+        int upchacne = Random.IntRange(0, 25);
 
         upchacne += Random.IntRange(-10, Dungeon.depth);
-        upchacne += Random.IntRange(-5, Dungeon.hero.belongings.armor.buffedLvl() * 3);
+        upchacne += Random.IntRange(-5, Dungeon.hero.belongings.armor != null ? Dungeon.hero.belongings.armor.buffedLvl() * 3 : 0);
 
-        if(upchacne > 35) n.level(4);
-        else if(upchacne > 20) n.level(3);
+        if (upchacne > 35) n.level(4);
+        else if (upchacne > 20) n.level(3);
         else if (upchacne > 12) n.level(2);
         else if (upchacne < -8) n.level(-2);
-        else if(upchacne < 0) n.level(1);
+        else if (upchacne < 0) n.level(1);
         else n.level(0);
 
 
