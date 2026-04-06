1ce81062b01dae7e132dce791f1625eee0eee139
Jun Lee <dan.junwoo.lee@gmail.com>
2026-04-06 02:45:08 -0400

    0.5.2: staff of snowsant fix (#35)

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/SP/StaffOfSnowsant.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/items/wands/SP/StaffOfSnowsant.java
@@ -8,7 +8,6 @@ import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Buff;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Silence;
 import com.shatteredpixel.shatteredpixeldungeon.actors.buffs.Slow;
 import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Hero;
-import com.shatteredpixel.shatteredpixeldungeon.actors.hero.Talent;
 import com.shatteredpixel.shatteredpixeldungeon.effects.Chains;
 import com.shatteredpixel.shatteredpixeldungeon.effects.Pushing;
 import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
@@ -18,16 +17,30 @@ import com.shatteredpixel.shatteredpixeldungeon.messages.Messages;
 import com.shatteredpixel.shatteredpixeldungeon.scenes.GameScene;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSprite;
 import com.shatteredpixel.shatteredpixeldungeon.sprites.ItemSpriteSheet;
+import com.shatteredpixel.shatteredpixeldungeon.tiles.DungeonTilemap;
 import com.watabou.noosa.audio.Sample;
 import com.watabou.utils.Callback;
 import com.watabou.utils.Random;
 
 public class StaffOfSnowsant extends Wand {
-    private int min(int lvl) { return 1 + lvl; }
-    private int max(int lvl) { return 7 + lvl * 5; }
-    private int min() { return min(buffedLvl()); }
-    private int max() { return max(buffedLvl()); }
+    private int min(int lvl) {
+        return 1 + lvl;
+    }
+
+    private int max(int lvl) {
+        return 7 + lvl * 5;
+    }
+
+    private int min() {
+        return min(buffedLvl());
+    }
+
+    private int max() {
+        return max(buffedLvl());
+    }
+
     private static ItemSprite.Glowing COL = new ItemSprite.Glowing(0xFF1493);
+
     {
         image = ItemSpriteSheet.WAND_SNOWSANT;
     }
@@ -38,14 +51,22 @@ public class StaffOfSnowsant extends Wand {
     }
 
     @Override
-    protected void onZap( Ballistica bolt ) {
+    protected void fx(Ballistica bolt, Callback callback) {
+        curUser.sprite.parent.add(new Chains(curUser.sprite.center(),
+                DungeonTilemap.raisedTileCenterToWorld(bolt.collisionPos),
+                callback));
+        Sample.INSTANCE.play(Assets.Sounds.ZAP);
+    }
 
-        Char ch = Actor.findChar( bolt.collisionPos );
+    @Override
+    protected void onZap(Ballistica bolt) {
+
+        Char ch = Actor.findChar(bolt.collisionPos);
         if (ch != null) {
 
 
             processSoulMark(ch, chargesPerCast());
-            Buff.affect(ch, Silence.class, 2f+buffedLvl());
+            Buff.affect(ch, Silence.class, 2f + buffedLvl());
 
             //bonus damage against INFECTED enemies
             if (ch.properties().contains(Char.Property.INFECTED)) {
@@ -59,7 +80,7 @@ public class StaffOfSnowsant extends Wand {
 
                 chainEnemy(bolt, curUser, ch);
             }
-            Sample.INSTANCE.play( Assets.Sounds.HIT_MAGIC, 1, Random.Float(0.65f, 0.75f) );
+            Sample.INSTANCE.play(Assets.Sounds.HIT_MAGIC, 1, Random.Float(0.65f, 0.75f));
 
             ch.sprite.burst(0xFFFFFFFF, buffedLvl() / 2 + 2);
 
@@ -70,8 +91,7 @@ public class StaffOfSnowsant extends Wand {
 
     @Override
     public void onHit(MagesStaff staff, Char attacker, Char defender, int damage) {
-        if (defender.buff(Silence.class) != null)
-        {
+        if (defender.buff(Silence.class) != null) {
             int dmg = damage / 5;
             defender.damage(dmg, attacker);
         }
@@ -79,7 +99,7 @@ public class StaffOfSnowsant extends Wand {
 
     private float slowChance() {
         if (buffedLvl() >= 10) return 1f;
-        return (float)(0.30 * Math.pow(10.0 / 3.0, buffedLvl() / 10.0));
+        return (float) (0.30 * Math.pow(10.0 / 3.0, buffedLvl() / 10.0));
     }
 
     private int slowChancePct() {
@@ -94,18 +114,18 @@ public class StaffOfSnowsant extends Wand {
             return Messages.get(this, "stats_desc", min(0), max(0), 30);
     }
 
-    private void chainEnemy(Ballistica chain, final Hero hero, final Char enemy ){
+    private void chainEnemy(Ballistica chain, final Hero hero, final Char enemy) {
 
         if (enemy.properties().contains(Char.Property.IMMOVABLE)) {
             return;
         }
 
         int bestPos = -1;
-        for (int i : chain.subPath(1, chain.dist)){
+        for (int i : chain.subPath(1, chain.dist)) {
             //prefer to the earliest point on the path
             if (!Dungeon.level.solid[i]
                     && Actor.findChar(i) == null
-                    && (!Char.hasProp(enemy, Char.Property.LARGE) || Dungeon.level.openSpace[i])){
+                    && (!Char.hasProp(enemy, Char.Property.LARGE) || Dungeon.level.openSpace[i])) {
                 bestPos = i;
                 break;
             }
@@ -117,22 +137,17 @@ public class StaffOfSnowsant extends Wand {
 
         final int pulledPos = bestPos;
 
-         Talent.onArtifactUsed(hero);
-         updateQuickslot();
+        updateQuickslot();
 
+        final int oldPos = enemy.pos;
+        enemy.pos = pulledPos;
+        Dungeon.level.occupyCell(enemy);
 
-        hero.busy();
-        hero.sprite.parent.add(new Chains(hero.sprite.center(), enemy.sprite.center(), new Callback() {
+        //push animation is processed by wandUsed()'s spendAndNext() since it's already in the actor queue
+        Actor.add(new Pushing(enemy, oldPos, pulledPos, new Callback() {
             public void call() {
-                Actor.add(new Pushing(enemy, enemy.pos, pulledPos, new Callback() {
-                    public void call() {
-                        enemy.pos = pulledPos;
-                        Dungeon.level.occupyCell(enemy);
-                        Dungeon.observe();
-                        GameScene.updateFog();
-                    }
-                }));
-                hero.next();
+                Dungeon.observe();
+                GameScene.updateFog();
             }
         }));
     }
