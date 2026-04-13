19f9cafb77264970a36e23c54f4e61f38ccdb80e
Jun Lee <dan.junwoo.lee@gmail.com>
2026-04-13 00:51:31 -0400

    0.5.2: rockfall trap fix

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/traps/RockfallTrap.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/levels/traps/RockfallTrap.java
@@ -97,9 +97,9 @@ public class RockfallTrap extends Trap {
 				damage -= ch.drRoll();
 				ch.damage( Math.max(damage, 0) , this);
 
-				Buff.prolong( ch, Paralysis.class, Paralysis.DURATION );
-
-				if (!ch.isAlive() && ch == Dungeon.hero){
+				if (ch.isAlive()) {
+					Buff.prolong( ch, Paralysis.class, Paralysis.DURATION );
+				} else if (ch == Dungeon.hero){
 					Dungeon.fail( getClass() );
 					GLog.n( Messages.get(this, "ondeath") );
 				}
